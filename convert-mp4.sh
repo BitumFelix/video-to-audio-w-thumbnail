a4fbc3275346ceb833508f9601369e431401ed60
BitumFelix <bitumfelix@gmail.com>
2020-12-27 12:47:36 +0300

    Image variables tested and working

--- convert-mp4.sh
+++ convert-mp4.sh
@@ -17,12 +17,13 @@ for f in *.mkv;
       ffmpeg -i "$f" -ss 00:00:18 -vframes 1 "${f%.*}".png
 
       # And save to variable image
-      # var image = "${f%.*}".png
+      image="${f%.*}".png
 
       # convert mp4 to mp3
       #ffmpeg -i "$f" -acodec libmp3lame -q:a 2 "${f%.*}.mp3";
 
       #embedd thumbnail to mp4
 
-      echo "finished converting and embedding thumbnail into mp3"
+      echo "finished converting $f and embedding thumbnail into mp3"
+      echo "$image"
 done
